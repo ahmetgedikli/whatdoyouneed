d02566cbff201bbeadc5522f2b60ec0a3bbef8bf
ahmet <ahmeetgedikli@gmail.com>
2022-01-01 23:18:26 +0300

    first commit

---
+++ app/src/main/java/com/example/whatdoyouneed/ui/services/ServicesFragment.kt
@@ -0,0 +1,40 @@
+package com.example.whatdoyouneed.ui.services
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.whatdoyouneed.databinding.FragmentServicesBinding
+
+class ServicesFragment : Fragment() {
+
+    private lateinit var servicesViewModel: ServicesViewModel
+    private var _binding: FragmentServicesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        servicesViewModel =
+                ViewModelProvider(this).get(ServicesViewModel::class.java)
+
+        _binding = FragmentServicesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
