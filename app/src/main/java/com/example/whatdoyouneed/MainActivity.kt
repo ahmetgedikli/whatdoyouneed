676a6d1bfee67c79321719d81e31b7726afc4643
Berkay <berkay.boz2@std.yeditepe.edu.tr>
2022-01-11 16:44:22 +0300

    Recycler View has been added

--- app/src/main/java/com/example/whatdoyouneed/MainActivity.kt
+++ app/src/main/java/com/example/whatdoyouneed/MainActivity.kt
@@ -7,20 +7,33 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.whatdoyouneed.databinding.ActivityMainBinding
+import com.example.whatdoyouneed.ui.services.Service
+import com.example.whatdoyouneed.ui.services.ServicesAdapter
+import com.example.whatdoyouneed.ui.services.ServicesFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    //lateinit var rv: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
 
+        //setContentView(R.layout.activity_main)
+        //val f1=ServicesFragment()
+        //rv= findViewById(R.id.rec_view)
+        //rv.layoutManager = LinearLayoutManager(this)
+        //val service: ArrayList<Service> = Arraylist<Service>()
+        //rv.adapter=ServicesAdapter()
+
+
+        val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
